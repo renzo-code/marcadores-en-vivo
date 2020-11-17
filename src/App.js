ce3dcf82226030d2a5e3f8b7ab91baa3c15a98b0
RenzoNeira <renzoneira@hotmail.com>
2020-11-17 18:37:55 -0500

    .

--- src/App.js
+++ src/App.js
@@ -10,219 +10,225 @@ class MarcadoresEnVivo extends React.Component{
   constructor(props) {
     super(props);
     this.state={
+      pais1: {},
+      pais2: {},
+      colorCabecera: {},
+      estadioPartido: '',
+
       listaMarcador: [],
       listaEstadioHora:{},
       tarjetasEquipo1: [],
       tarjetasEquipo2: [],
-      goleadoresEquipo1: [],
-      goleadoresEquipo2: [],
-      cronometro: '0:00'
+      goleadoresEquipo1: '',
+      goleadoresEquipo2: '',
+      cronometro: '0:00',
+      cronometro2: '45:00',
+      time: 1,
     }
   }
 
-
   componentDidMount(){
     this.obtenerEquiposMarcador()
-    this.obtenerEstadioHora()
-    this.obtenerTarjetas()
-    this.obtenerGoleadores()
-    let actual = moment();
-    let lunch1 = moment('2020-11-13T16:33:00');
-    let min = actual.diff(lunch1, 'minute');
-    // let seg = actual.diff(lunch1, 's');
-    
-    console.log('info', moment().second())
-
-    this.setState({
-      cronometro: `${min}:${moment().second()}`
-    })
-    
-    this.interval = setInterval(() => this.tick(), 1000);
   }
 
-  tick() {
-    const { cronometro } = this.state
+  tick(hour) {
+    const { cronometro, time } = this.state
+
+    const lunch1 = moment(hour);
+    const diff = moment().diff(lunch1, 'minute');
+
+    // console.log('min', diff , hour)
+    if (diff < 0) {
+      this.setState({
+        cronometro: lunch1.format('h:mm')
+      })
+      return
+    }
 
     const min = parseInt(cronometro.split(':')[0])
-    const seg = parseInt(cronometro.split(':')[1])
+    console.log('min', min)
 
     let date = '00:00'
+    const seg = parseInt(cronometro.split(':')[1])
     if (seg >= 59) {
       date = `${min + 1}:00`
     } else {
       let seg2 = seg + 1
-      date = `${min}:${seg2 < 10 ? `0${seg2}` : seg2}`
+      date = `${min < 10 ? `0${min}` : min}:${seg2 < 10 ? `0${seg2}` : seg2}`
     }
 
-    if (min >= 45) date = `45:00 +5`
+    if (min >= 45 && time === 'FIRST_TIME') date = `45:00 +`
+
+    if (min >= 90 && time === 'SECOND_TIME') date = `90:00 +`
 
     this.setState({
       cronometro: date
     })
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
   obtenerEquiposMarcador = async () => {
     try{
-      const dataEquiposMarcador = await Axios.get(equiposMarcador)
-      this.setState({
-        listaMarcador : this.formatDatosEquiposMarcador(dataEquiposMarcador.data.values)
-      })
-    }
-    catch(e){
-      console.error(e)
-    }
-  }
-
-  obtenerEstadioHora = async () => {
-    try{
-      const dataEstadioHora = await Axios.get(estadioHora)
-      this.setState({
-        listaEstadioHora : this.formatEstadoHora(dataEstadioHora.data.values)
-      })
-    }
-    catch(e){
-      console.error(e)
-    }
-  }
-
-  obtenerTarjetas = async () => {
-    try{
-      const dataTarjetaAmarilla = await Axios.get(tarjetas)
-      this.formatTarjetas(dataTarjetaAmarilla.data.values)
-      
-    }
-    catch(e){
-      console.error(e)
-    }
-  }
-
-  obtenerGoleadores = async () => {
-    try{
-      const dataTarjetaRoja = await Axios.get(goleadores)
-      this.formatGoleadores(dataTarjetaRoja.data.values)
-    }
-    catch(e){
-      console.error(e)
-    }
-  }
-
-  formatDatosEquiposMarcador = (dates) => {
-    const info = []
-    let names = null
-    dates.forEach((item, i) => {
-      if (i === 0) {
-        names = item
-      } else {
-        info.push({
-          [names[0]]: item[0],
-          [names[1]]: item[1],
-          [names[2]]: item[2],
+      const options =  {
+        method : 'GET',
+        headers: {'token_id' : '0D93FB225g7592Dh3gG1D5G61f35H69Ga728C3DE7F'},
+        // url : 'https://cronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=5fb3fb7638b5c01d5e74a9d2&no-api-cache=1&status=1',
+        url: 'https://cronosservices.glr.pe/api/spotlight?site_id=larepublica&_id=5fb3fb7638b5c01d5e74a9d2&no-api-cache=1&no-cache=1&status=1'
+      }
+        const dataEquiposMarcador = await Axios(options)
+        console.log('asd', dataEquiposMarcador.data.data.spotlight.data)
+        
+        this.setState({
+          pais1 : dataEquiposMarcador.data.data.spotlight.data[0].data[0].fields,
+          pais2 : dataEquiposMarcador.data.data.spotlight.data[0].data[1].fields,
+          cabeceraEnVivo: dataEquiposMarcador.data.data.spotlight.data[3].data[0].value,
+          colorCabecera: dataEquiposMarcador.data.data.spotlight.data[8].data[0].value,
+          estadioPartido: dataEquiposMarcador.data.data.spotlight.data[4].data[0].value,
         })
-      }
-    })
-    return info
+        this.obtenerTiempoJuego(dataEquiposMarcador.data.data.spotlight.data[5].data[0].value)
+        this.convertirDataGoleadores(dataEquiposMarcador.data.data.spotlight.data[1].data)
+        this.iniciarCronometro(dataEquiposMarcador.data.data.spotlight.data[6].data[0].value)
+        this.convertirDataTarjetas(dataEquiposMarcador.data.data.spotlight.data[2].data)
+    }
+    catch(e){
+      console.error(e)
+    }
   }
 
-  formatEstadoHora = (data) => {
-    let info = {}
-    let names = null
-    data.forEach((item, i) => {
-      if (i === 0) {
-        names = item
-      } else {
-        info = {
-          [names[0]]: item[0],
-          [names[1]]: item[1],
-          [names[2]]: item[2],
-          [names[3]]: item[3],
-          [names[4]]: item[4]
-        }
-      }
-    })
-    // let lunch1 = moment(info.hora);
-    // let actual = moment();
-    // let lunch1 = moment('2020-11-13T16:00:00');
-    // let min = actual.diff(lunch1, 'minute');
-    // let seg = actual.diff(lunch1, 'seconds');
-    
-    // console.log('info', min, seg)
-    // this.setState({
-    //   cronometro: `${min}:${seg}`
-    // })
-    
-    // this.interval = setInterval(() => this.tick(), 1000);
-    return info
-  }
+  iniciarCronometro = (date) => {
+    const { time } = this.state
+    console.log('date', date , time)
+    let hour = date
 
-  formatTarjetas = (data) => {
-    let equipo1 = []
-    let equipo2 = []
-    let names = null
-    data.forEach((item, i) => {
-      if (i === 0) names = item
-      if (i !== 0) {
-        if (item[2] === '1') {
-          equipo1.push({
-            [names[0]]: item[0],
-            [names[1]]: item[1],
-            [names[2]]: item[2],
-          })
-        } else {
-          equipo2.push({
-            [names[0]]: item[0],
-            [names[1]]: item[1],
-            [names[2]]: item[2],
-          })
-        }
-      }
-    })
+    if (time === 'SECOND_TIME') {
+      hour = moment(date, 'YYYY-MM-DD hh:mm:ss').add(15, 'minute').format('YYYY/MM/DDTHH:mm:ss')
+    }
+
+    console.log('hour', hour)
+    let actual = moment();
+    let lunch1 = moment(hour, 'YYYY-MM-DD hh:mm:ss');
+    let min = actual.diff(lunch1, 'minutes');
+    console.log('lunch1', lunch1, hour)
+
     this.setState({
-      tarjetasEquipo1: equipo1,
-      tarjetasEquipo2: equipo2
+      cronometro: min >= 0 ? `${min}:${moment().second()}` : moment(hour, 'YYYY-MM-DD hh:mm:ss').format('h:mm A')
+    })
+
+    if (min >= 0) {
+      this.interval = setInterval(() => this.tick(lunch1), 1000);
+    }
+  }
+
+  convertirDataGoleadores = (array) => {
+    let goleadoresEquipo1 = ''
+    let goleadoresEquipo2 = ''
+    array.forEach((item, i) => {
+      if (item.fields[1].value['1']) {
+        goleadoresEquipo1 === ''
+          ? goleadoresEquipo1 = item.fields[0].value
+          : goleadoresEquipo1 += ` - ${item.fields[0].value}`
+      }
+      if (item.fields[1].value['2']) {
+        goleadoresEquipo2 === ''
+        ? goleadoresEquipo2 = item.fields[0].value
+        : goleadoresEquipo2 += ` - ${item.fields[0].value}`
+      }
+    })
+
+    this.setState({
+      goleadoresEquipo1,
+      goleadoresEquipo2
     })
   }
 
-  formatGoleadores = (data) => {
-    let equipo1 = []
-    let equipo2 = []
-    let names = null
-    data.forEach((item, i) => {
-      if (i === 0) names = item
-      if (i !== 0) {
-        if (item[1] === '1') {
-          equipo1.push({
-            [names[0]]: item[0],
-            [names[1]]: item[1],
-          })
-        } else {
-          equipo2.push({
-            [names[0]]: item[0],
-            [names[1]]: item[1],
-          })
+  obtenerTiempoJuego = (time) => {
+    console.log('time', time)
+
+    if (time.first_time) {
+      this.setState({
+        time: 'FIRST_TIME'
+      })
+    } else if (time.second_time) {
+      this.setState({
+        time: 'SECOND_TIME'
+      })
+    } else if (time.first_overtime) {
+      this.setState({
+        time: 'FIRST_OVERTIME'
+      })
+    } else if (time.second_overtime) {
+      this.setState({
+        time: 'SECOND_OVERTIME'
+      })
+    }
+  }
+
+  convertirDataTarjetas = (array) => {
+    let tarjetasRojasEquipo1 = ''
+    let tarjetasAmarillasEquipo1 = ''
+    let tarjetasRojasEquipo2 = ''
+    let tarjetasAmarillasEquipo2 = ''
+
+    array.forEach((item) => {
+      if (item.fields[2].value['1']) {
+        if (item.fields[1].value['yellow']) {
+          tarjetasAmarillasEquipo1 === ''
+            ? tarjetasAmarillasEquipo1 = item.fields[0].value
+            : tarjetasAmarillasEquipo1 += ` - ${item.fields[0].value}`
+        }
+        if (item.fields[1].value['red']) {
+          tarjetasRojasEquipo1 === ''
+            ? tarjetasRojasEquipo1 = item.fields[0].value
+            : tarjetasRojasEquipo1 += ` - ${item.fields[0].value}`
+        }
+      }
+      if (item.fields[2].value['2']) {
+        if (item.fields[1].value['yellow']) {
+          tarjetasAmarillasEquipo2 === ''
+          ? tarjetasAmarillasEquipo2 = item.fields[0].value
+          : tarjetasAmarillasEquipo2 += ` - ${item.fields[0].value}`
+        }
+        if (item.fields[1].value['red']) {
+          tarjetasRojasEquipo2 === ''
+          ? tarjetasRojasEquipo2 = item.fields[0].value
+          : tarjetasRojasEquipo2 += ` - ${item.fields[0].value}`
         }
       }
     })
+
+    // console.log('goleadoresR1', tarjetasRojasEquipo1)
+    // console.log('goleadoresA1', tarjetasAmarillasEquipo1)
+    // console.log('goleadoresR2', tarjetasRojasEquipo2)
+    // console.log('goleadoresA2', tarjetasAmarillasEquipo2)
+
     this.setState({
-      goleadoresEquipo1 : equipo1,
-      goleadoresEquipo2 : equipo2
+      tarjetasEquipo1: [tarjetasRojasEquipo1, tarjetasAmarillasEquipo1],
+      tarjetasEquipo2: [tarjetasRojasEquipo2, tarjetasAmarillasEquipo2]
     })
-    return equipo1,equipo2
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+    
   }
 
   render(){
     const {
+      pais1,
+      pais2,
+      cabeceraEnVivo,
+      colorCabecera,
+      estadioPartido,
+
       listaMarcador,
       tarjetasEquipo2,
       tarjetasEquipo1,
       listaEstadioHora,
       goleadoresEquipo1,
       goleadoresEquipo2,
-      cronometro
+      cronometro,
     } = this.state
+    
+    // console.log('goleadoresEquipo2', moment().add(15, 'minutes').format('YYYY-MM-DD hh:mm:ss'))
 
   return(
     <>
@@ -230,17 +236,17 @@ class MarcadoresEnVivo extends React.Component{
         <ContainerMaster>
           <div>
             <JugadorTarjeta color="red">
-              {tarjetasEquipo1[0]?.jugador && <p></p>}
+              {tarjetasEquipo1[0] !== '' && <p></p>}
               {
-                tarjetasEquipo1[0]?.jugador.split('-').map(p => (
+                tarjetasEquipo1[0]?.split('-').map(p => (
                   <div>{p}</div>
                 ))
               }
             </JugadorTarjeta>
             <JugadorTarjeta color="yellow">
-            {tarjetasEquipo1[1]?.jugador && <p></p>}
+            {tarjetasEquipo1[1] !== '' && <p></p>}
               {
-                tarjetasEquipo1[1]?.jugador.split('-').map(p => (
+                tarjetasEquipo1[1]?.split('-').map(p => (
                   <div>{p}</div>
                 ))
               }
@@ -250,9 +256,9 @@ class MarcadoresEnVivo extends React.Component{
             {goleadoresEquipo1.length !== 0 &&<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQL8W6ZoTabIuzsZ_Jmn_hGISkqOrnyZFsA7A&usqp=CAU" />}
             <div>
               {
-                goleadoresEquipo1.map(item => (
+                goleadoresEquipo1.split('-').map(jugador => (
                   <div>
-                    {item.jugador}
+                    {jugador}
                   </div>
                 ))
               }
@@ -261,31 +267,31 @@ class MarcadoresEnVivo extends React.Component{
           <ContainerEquiposMarcador>
             <WrapperPais>
               <ImgPais 
-                src={listaMarcador[0]?.imagen}
+                src={pais1[1]?.value.url}
               />
-              <Pais>{listaMarcador[0]?.equipos}</Pais>
+              <Pais> {pais1[0]?.value} </Pais>
             </WrapperPais>
-            <Marcador>{listaMarcador[0]?.marcador}</Marcador>
+            <Marcador>{pais1[3]?.value}</Marcador>
             <EnVivo>
-              <TituloPrincipal estadoColor={listaEstadioHora.estado_color} >{listaEstadioHora.estado}</TituloPrincipal>
+              <TituloPrincipal estadoColor={colorCabecera}>{cabeceraEnVivo}</TituloPrincipal>
               <TiempoPartido>{cronometro}</TiempoPartido>
-              <Estadio>{listaEstadioHora.estadio}</Estadio>
+              <Estadio>{estadioPartido}</Estadio>
             </EnVivo>
-            <Marcador>{listaMarcador[1]?.marcador}</Marcador>
+            <Marcador>{pais2[3]?.value}</Marcador>
             <WrapperPais>
               <ImgPais
-                src={listaMarcador[1]?.imagen}
+                src={pais2[1]?.value.url}
               />
-              <Pais>{listaMarcador[1]?.equipos}</Pais>
+              <Pais>{pais2[0]?.value}</Pais>
             </WrapperPais>
           </ContainerEquiposMarcador>
             <Goleadores right="35px">
-              {goleadoresEquipo2.length !== 0 &&<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQL8W6ZoTabIuzsZ_Jmn_hGISkqOrnyZFsA7A&usqp=CAU" />}
+              {goleadoresEquipo2.length !== 0 &&<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQL8W6ZoTabIuzsZ_Jmn_hGISkqOrnyZFsA7A&usqp=CAU"/>}
               <div>
                 {
-                  goleadoresEquipo2.map(item => (
+                  goleadoresEquipo2.split('-').map(jugador => (
                     <div>
-                      {item.jugador}
+                      {jugador}
                     </div>
                   ))
                 }
@@ -293,17 +299,17 @@ class MarcadoresEnVivo extends React.Component{
             </Goleadores>
             <div>
               <JugadorTarjeta color="yellow">
-                {tarjetasEquipo2[1]?.jugador && <p></p>}
+                {tarjetasEquipo2[1] !== '' && <p></p>}
                 {
-                  tarjetasEquipo2[1]?.jugador.split('-').map(p => (
+                  tarjetasEquipo2[1]?.split('-').map(p => (
                     <div>{p}</div>
                   ))
                 }
               </JugadorTarjeta>
               <JugadorTarjeta color="red">
-              {tarjetasEquipo2[0]?.jugador && <p></p>}
+              {tarjetasEquipo2[0] !== '' && <p></p>}
                 {
-                  tarjetasEquipo2[0]?.jugador.split('-').map(p => (
+                  tarjetasEquipo2[0]?.split('-').map(p => (
                     <div>{p}</div>
                   ))
                 }
@@ -385,7 +391,7 @@ const TituloPrincipal = styled.h2`
 `
 const TiempoPartido = styled.h1`
   font-family: 'Roboto', sans-serif;
-  font-size: 23px;
+  font-size: 20px;
   text-align: center;
   
 `
